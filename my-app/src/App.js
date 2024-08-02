424861be68d605d74344b4c2bb10a07a279329d4
Alsona Natsi <alsona.naci1188@gmail.com>
2024-08-02 02:41:12 +0000

    Creating the search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
+import { SearchBar } from './components/SearchBar';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       <NavBarForm />
+      <Sidebar />
+      <Content />
+      
+
     </div>
   );
 }
